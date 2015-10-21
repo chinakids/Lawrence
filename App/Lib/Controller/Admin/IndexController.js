f7398f05ad42855c436320317c9902c7a48306a0
chinakids <494063570@qq.com>
2015-10-21 20:28:25 +0800

    测试数据库读取

--- App/Lib/Controller/Admin/IndexController.js
+++ App/Lib/Controller/Admin/IndexController.js
@@ -6,6 +6,8 @@ module.exports = Controller("Home/BaseController", function(){
   "use strict";
   return {
     indexAction: function(){
+      var model = D('AdminUser');
+      console.log(model.getAdmin())
       //render View/Home/index_index.html file
       this.display();
     }
---
+++ App/Lib/Model/AdminUserModel.js
@@ -0,0 +1,12 @@
+//模型定义
+module.exports = Model(function () {
+    return {
+        //获取管理员用户列表
+        getAdmin: function () {
+            //实例化模型类
+            return this.order('id').select().then(function(data) {
+                return data;
+            });
+        }
+    }
+});
