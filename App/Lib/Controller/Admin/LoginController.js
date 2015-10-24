360b39a9a466bb2c468340de9247bc6c4d98f063
chinakids <494063570@qq.com>
2015-10-24 11:46:23 +0800

    后台登陆流程80%

--- App/Lib/Controller/Admin/LoginController.js
+++ App/Lib/Controller/Admin/LoginController.js
@@ -10,9 +10,25 @@ module.exports = Controller("Home/BaseController", function(){
       this.display();
     },
     loginAction: function(){
-      if(this.isPost()){
-        this.end(md5(md5('pengyujie')));
+      var that = this;
+      if(that.isPost()){
+        // this.end(md5(md5('pengyujie')));
         //console.log('111')
+        var req = that.post();
+        Promise.all([D('AdminUser').getUserByName(req.username)]).then(function(data){
+          console.log(data)
+          if(data[0].name == undefined){
+            that.error(1001,'该帐号不存在').end()
+          }
+          if(data[0].status == 0){
+            that.error(1002,'该帐号已经被禁用').end()
+          }
+          if(data[0].password != md5(md5(req.password))){
+            that.error(1003,'密码错误').end()
+          }
+          //以下为登陆陈公共能
+          that.success({msg:'登陆成功'}).end()
+        });
       }
     }
   };
--- App/Lib/Model/AdminUserModel.js
+++ App/Lib/Model/AdminUserModel.js
@@ -2,11 +2,16 @@
 module.exports = Model(function () {
     return {
         //获取管理员用户列表
-        getAdmin: function () {
-            //实例化模型类
-            return this.select().then(function(data) {
-                return data;
-            })
+        getLists: function () {
+          //实例化模型类
+          return this.select().then(function(data) {
+              return data;
+          })
+        },
+        getUserByName: function(name){
+          return this.where({name: name}).find().then(function(data) {
+              return data || {};
+          })
         }
     }
 });
