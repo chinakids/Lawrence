cc11a8d73c2d174318ab2e5072d7288306f6806c
chinakids <494063570@qq.com>
2015-10-29 20:30:21 +0800

    增加文章模型

--- App/Lib/Model/ArticleModel.js
+++ App/Lib/Model/ArticleModel.js
@@ -1,16 +1,10 @@
 //模型定义
 module.exports = Model(function () {
   return {
-    //获取管理员用户列表
     getLists: function () {
       //实例化模型类
       return this.select().then(function(data) {
         return data;
-      })
-    },
-    getUserByName: function(name){
-      return this.where({name: name}).find().then(function(data) {
-        return data || {};
       })
     }
   }
