d864729c8c901e4bb8c83f35957c286bc3d28c99
chinakids <494063570@qq.com>
2015-12-02 19:29:24 +0800

    增加 js

--- html/resource/coffee/common.coffee
+++ html/resource/coffee/common.coffee
@@ -2,3 +2,7 @@ $ () ->
   WH = $(window).height()
   #设置侧边栏的高度
   $('.nav').height(WH);
+  #初始化瀑布流
+  waterfall = new Waterfall
+    test : false
+    between : 15
--- html/resource/js/common.js
+++ html/resource/js/common.js
@@ -1,3 +1,3 @@
-/*! Lawrence - v0.0.1 - 2015-11-30 */
-(function(){$(function(){var a;return a=$(window).height(),$(".nav").height(a)})}).call(this);
+/*! Lawrence - v0.0.1 - 2015-12-02 */
+(function(){$(function(){var a,b;return a=$(window).height(),$(".nav").height(a),b=new Waterfall({test:!1,between:15})})}).call(this);
 //# sourceMappingURL=common.js.map
